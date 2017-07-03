ca67d682e7fc480768a1d56398991d268e610c0b
M_DESKTOP-RBQ57OU\User <name368368@gmail.com>
2017-07-03 21:55:48 +0200

    com 33

--- BattleTanks 4.16/Source/BattleTanks/Public/TankTrack.h
+++ BattleTanks 4.16/Source/BattleTanks/Public/TankTrack.h
@@ -26,4 +26,8 @@ public:
 
 private:
 	UTankTrack();
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComponent, FVector NormalImpulse, const FHitResult& Hit);
 };
